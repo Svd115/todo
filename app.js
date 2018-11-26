e1dbd66cc186f0df54bd0d1e3399a7d91267b81b
Svd115 <svd115@yahoo.fr>
2018-11-26 15:43:44 +0100

    'h'

--- app.js
+++ app.js
@@ -13,7 +13,30 @@
 		res.render('../views/page.ejs');
 	})
 	.post('/', urlencodedParser, function(req, res){
-		
+		var SparkPost = require('sparkpost');
+		var sparky = new SparkPost(); // uses process.env.SPARKPOST_API_KEY
+
+		sparky.transmissions.send({
+			options: {
+			  sandbox: true
+			},
+			content: {
+			  from: 'testing@' + process.env.SPARKPOST_SANDBOX_DOMAIN, // 'testing@sparkpostbox.com'
+			  subject: 'Oh hey!',
+			  html:'<html><body><p>Testing SparkPost - the world\'s most awesomest email service!</p><p>'+req.body.mail+'</p></body></html>'
+			},
+			recipients: [
+			  {address: req.body.adress}
+			]
+		  })
+		  .then(data => {
+			console.log('Woohoo! You just sent your first mailing!');
+			console.log(data);
+		  })
+		  .catch(err => {
+			console.log('Whoops! Something went wrong');
+			console.log(err);
+		  });
 		
 		res.redirect('/');
 	})
